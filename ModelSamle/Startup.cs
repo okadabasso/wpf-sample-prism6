f8b50edc8261a66e9a3a965b0ccc891d559c9dbf
okadabasso <okadabasso@gmail.com>
2020-08-28 11:17:21 +0900

    モデルの注入にモジュールを使うようにする

--- ModelSamle/Startup.cs
+++ ModelSamle/Startup.cs
@@ -40,24 +40,8 @@ namespace ModelSamle
             builder.RegisterType<TraceInterceptor>();
             builder.RegisterType<TransactionInterceptor>();
 
-            builder.Register<NorthwindDbContext>((c) => {
-                var db = new NorthwindDbContext();
-                var parameter = new PositionalParameter(0, typeof(NorthwindDbContext).FullName);
-                var logger = c.Resolve<ILogger>(parameter);
-                db.Database.Log = sql =>
-                {
-                    if (logger.IsTraceEnabled)
-                    {
-                        var callerClassName = typeof(NorthwindDbContext).FullName;
-                        NLog.LogEventInfo info = new NLog.LogEventInfo(NLog.LogLevel.Trace, logger.Name, sql);
-                        // 呼び出し元情報を設定します。
-                        info.SetCallerInfo(callerClassName, null, null, 0);
-                        logger.Log(typeof(ModelsDependencyModule), info);
+            builder.RegisterModule<ModelsDependencyModule>();
 
-                    }
-                };
-                return db;
-            });
 
 
 
