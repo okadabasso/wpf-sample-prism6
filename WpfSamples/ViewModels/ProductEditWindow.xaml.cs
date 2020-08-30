3482c8c628658ec91dedb2a71d64bf2e3f05ce07
okadabasso <okadabasso@gmail.com>
2020-08-31 08:50:19 +0900

    編集画面を追加する

---
+++ WpfSamples/ViewModels/ProductEditWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfSamples.ViewModels
+{
+    /// <summary>
+    /// Interaction logic for ProductEditWindow.xaml
+    /// </summary>
+    public partial class ProductEditWindow : Window
+    {
+        public ProductEditWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
