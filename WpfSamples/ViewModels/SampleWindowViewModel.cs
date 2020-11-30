abaa33659f765ab23eac3f51e346d6e7ca2f73fa
okadabasso <okadabasso@gmail.com>
2020-11-30 10:01:53 +0900

    proxy実験のための修正

--- WpfSamples/ViewModels/SampleWindowViewModel.cs
+++ WpfSamples/ViewModels/SampleWindowViewModel.cs
@@ -15,121 +15,95 @@ using System.Windows.Media.Animation;
 using WpfSamples.Infrastructure.ComponentManagement.Attributes;
 using WpfSamples.Infrastructure.Logging;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel;
 
 namespace WpfSamples.ViewModels
 {
-    public class BaseViewModel : BindableBase
+    public class BaseViewModel : INotifyPropertyChanged
     {
-        private bool _v;
-        public bool v
-        {
-            get { return _v; }
-            set { SetProperty(ref _v, value); }
-        }
-
+        public event PropertyChangedEventHandler PropertyChanged;
         public BaseViewModel()
         {
         }
-    }
-    public class SampleWindowViewModel : BaseViewModel, IInteractionRequestAware
-    {
-        private bool _closeWindow = false;
-        public bool CloseWindow
+        protected void SetProperty<T>(ref T field, T value, [CallerMemberName] String propertyName = "")
         {
-            get { return _closeWindow; }
-            set { SetProperty(ref _closeWindow, value); }
-
+            field = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
-        public DelegateCommand CloseCommand { get; set; }
-        private readonly IContainer _container;
+    }
+    [DependencyObject]
+    public class SampleWindowViewModel : BindableBase, IInteractionRequestAware
+    {
+
+        private readonly Autofac.IContainer _container;
         private readonly ILogger _logger;
+
+        public DelegateCommand CloseCommand { get; set; }
         public DelegateCommand SubmitCommand { get; set; }
         public DelegateCommand ChangeCommand { get; set; }
         private INotification _notification;
         public INotification Notification
         {
-            get 
-            { 
-                return _notification; 
-            }
-            set
-            {
-                this._notification = value;
-            }
+            get =>  _notification; 
+            set => this._notification = value;
+            
         }
-
         public Action FinishInteraction { get; set; }
 
-        public DelegateCommand LoadedAction { get; set; }
+        protected int _x;
+        public int X
+        {
+            get => _x;
+            set
+            {
+                SetProperty(ref _x, value);
+                UpdateResult();
+                RaisePropertyChanged(nameof(X));
+            }
+        }
+        protected int _y;
+        public int Y
+        {
+            get => _y;
+            set
+            {
+                SetProperty(ref _y, value);
+                UpdateResult();
+                RaisePropertyChanged(nameof(Y));
+            }
+        }
+        protected int _result;
 
-        public ReactiveProperty<bool> watcher { get; set; }
-        public ReactiveProperty<int> x { get; set; } = new ReactiveProperty<int>();
-        public ReactiveProperty<int> y { get; set; } = new ReactiveProperty<int>();
-        public ReactiveProperty<int> result { get; set; } = new ReactiveProperty<int>();
-        public SampleWindowViewModel (IContainer container, ILogger<SampleWindowViewModel> logger) : base()
+
+        public int Result { get => _result; set => SetProperty(ref _result, value); }
+        private void UpdateResult()
+        {
+            Result = X + Y;
+        }
+
+        public SampleWindowViewModel (Autofac.IContainer container, ILogger<SampleWindowViewModel> logger) : base()
         {
             _container = container;
             _logger = logger;
             SubmitCommand = new DelegateCommand(Close);
-            LoadedAction = new DelegateCommand(OnLoaded);
             ChangeCommand = new DelegateCommand(Change);
             CloseCommand = new DelegateCommand(Close);
-
-            v = true;
-            CloseWindow = false;
-            result = x.CombineLatest(y, (x, y) => x + y).ToReactiveProperty();
-            watcher = this.ObserveProperty(x => x.CloseWindow).ToReactiveProperty();
-            watcher.Subscribe(_ => RaisePropertyChanged("CloseWindow"));
-
-            RaisePropertyChanged();
-            RaisePropertyChanged("CloseWindow");
+            RaisePropertyChanged(null);
         }
         [Trace]
-        protected virtual void OnLoaded()
-        {
-
-        }
         protected virtual void Change()
         {
-            v = !v;
+            X++;
         }
         [Trace]
         protected virtual void Close()
         {
-            CloseWindow = v;
+            FinishInteraction();
 
         }
 
-    }
-    public class CloseWindowAttachedBehavior
-    {
-        public static bool GetClose(DependencyObject obj)
-        {
-            return (bool)obj.GetValue(CloseProperty);
-        }
-
-        public static void SetClose(DependencyObject obj, bool value)
-        {
-            obj.SetValue(CloseProperty, value);
-        }
-
-        // Using a DependencyProperty as the backing store for Close.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty CloseProperty =
-            DependencyProperty.RegisterAttached("Close", typeof(bool), typeof(CloseWindowAttachedBehavior), new PropertyMetadata(false, OnCloseChanged));
-
-        private static void OnCloseChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (!(d is Window win))
-            {
-                // Window以外のコントロールにこの添付ビヘイビアが付けられていた場合は、
-                // コントロールの属しているWindowを取得
-                win = Window.GetWindow(d);
-            }
-
-            if (GetClose(d))
-            {
-                    win.Close();
-            }
-        }
     }
 }
