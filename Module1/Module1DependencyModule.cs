39065d77f53ae5d0235d02bc6a2798ff3228e5f9
okadabasso <okadabasso@gmail.com>
2021-01-02 14:54:48 +0900

    Moduleに定義されたView をnavigatableにする

--- Module1/Module1DependencyModule.cs
+++ Module1/Module1DependencyModule.cs
@@ -5,17 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Prism.Autofac;
-
+using System.Reflection;
 namespace Module1
 {
     public class Module1DependencyModule : Autofac.Module
     {
+        private Action<Type, string> register;
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
 
-            builder.RegisterTypeForNavigation<Views.ViewA>(typeof(Views.ViewA).FullName);
-            builder.RegisterTypeForNavigation<Views.ViewB>(typeof(Views.ViewB).FullName);
+
+            var types = this.GetType().Assembly.GetTypes().Where(x => x.Namespace.EndsWith("Views"));
+            foreach(var type in types)
+            {
+                var method = CreateRegisterViewMethod(type);
+                method.Invoke(builder, new object[] {builder, type.FullName });
+            }
+        }
+        private static MethodInfo registerMethod = typeof(AutofacExtensions).GetMethod("RegisterTypeForNavigation");
+        private MethodInfo CreateRegisterViewMethod(Type viewType)
+        {
+            var genericMethod = registerMethod.MakeGenericMethod(viewType);
+            return genericMethod;
         }
     }
 }
