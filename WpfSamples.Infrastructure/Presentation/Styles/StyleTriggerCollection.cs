2881339881b3502c14cd39f303ae9e12c3040c4e
michiakiokada <michiakiokada@craft-net.co.jp>
2021-01-13 15:16:20 +0900

    styleにeventtriggerを追加するための仕掛け

--- WpfSamples.Infrastructure/Presentation/Styles/StyleTriggerCollection.cs
+++ WpfSamples.Infrastructure/Presentation/Styles/StyleTriggerCollection.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WpfSamples.Styles
+namespace WpfSamples.Infrastructure.Presentation.Styles
 {
     public class StyleTriggerCollection : FreezableCollection<System.Windows.Interactivity.TriggerBase>
     {
