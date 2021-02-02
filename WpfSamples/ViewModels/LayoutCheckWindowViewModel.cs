63fe8dd952d5deb862152116f6d323230257e2bd
micokada <okadabasso@gmail.com>
2021-02-02 20:18:16 +0900

    画面遷移でデータグリッドがウインドウの大きさに追従するかテストする
    Grid で表示非表示を切り替えたらだめ

---
+++ WpfSamples/ViewModels/LayoutCheckWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using WpfSamples.Models;
+using WpfSamples.Models.Entities;
+
+namespace WpfSamples.ViewModels
+{
+	public class LayoutCheckWindowViewModel : BindableBase
+	{
+        public ReactiveCommand LoadedCommand { get; set; } = new ReactiveCommand();
+        public ObservableCollection<Category> Categories { get; set; }
+        public ReactiveProperty<Visibility> CategoryPanelVisibility { get; set; } = new ReactiveProperty<Visibility>();
+        public ReactiveProperty<int> SelectedCategoryId { get; set; } = new ReactiveProperty<int>();
+
+        public ReactiveProperty<Visibility> ProductListVisibility { get; set; } = new ReactiveProperty<Visibility>();
+        public ObservableCollection<Product> Products { get; set; }
+
+        public ReactiveCommand NavigateNext { get; set; } = new ReactiveCommand();
+        public ReactiveCommand ExitView2Command { get; set; } = new ReactiveCommand();
+
+        public LayoutCheckWindowViewModel()
+        {
+            LoadedCommand.Subscribe(()=> {
+                using(var context = new NorthwindDbContext())
+                {
+                    Categories = new ObservableCollection<Category>(context.Categories.ToList());
+                    CategoryPanelVisibility.Value = Visibility.Visible;
+                    ProductListVisibility.Value = Visibility.Collapsed;
+
+                    RaisePropertyChanged(null);
+                }
+            });
+            NavigateNext.Subscribe(() => {
+                using (var context = new NorthwindDbContext())
+                {
+                    Products = new ObservableCollection<Product>(context.Products.Where(x => x.CategoryId == SelectedCategoryId.Value).ToList());
+                    CategoryPanelVisibility.Value = Visibility.Collapsed;
+                    ProductListVisibility.Value = Visibility.Visible;
+
+                    RaisePropertyChanged(null);
+                }
+
+            });
+            ExitView2Command.Subscribe(() => {
+                CategoryPanelVisibility.Value = Visibility.Visible;
+                ProductListVisibility.Value = Visibility.Collapsed;
+
+            });
+        }
+
+    }
+}
---
+++ WpfSamples/Views/LayoutCheckWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfSamples.Views
+{
+    /// <summary>
+    /// Interaction logic for LayoutCheckWindow.xaml
+    /// </summary>
+    public partial class LayoutCheckWindow : Window
+    {
+        public LayoutCheckWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
